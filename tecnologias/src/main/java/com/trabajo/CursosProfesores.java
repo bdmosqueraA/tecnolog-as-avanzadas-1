f2b730b5b4df9fc74af4c402e5828b77fe3c386e
JeOsorio23 <jeosorio@unillanos.edu.co>
2025-02-18 11:28:31 -0500

    Delete tecnologias directory

--- tecnologias/src/main/java/com/trabajo/CursosProfesores.java
+++
@@ -1,7 +0,0 @@
-package com.trabajo;
-
-import java.util.List;
-
-public class CursosProfesores {
-    private List<Inscripcion> listado;
-}
--- tecnologias/src/main/java/com/trabajo/Servicios.java
+++
@@ -1,9 +0,0 @@
-package com.trabajo;
-
-import java.util.List;
-
-public interface Servicios {
-    public String imprimirPosicion(String posicion);
-    public int cantidadActual();
-    public List<String> imprimirListado();
-}
